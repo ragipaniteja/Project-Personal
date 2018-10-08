940acc5493af49a81d3a6f12b1211577b9c95f78
ragipaniteja <ragipaniteja93@gmail.com>
2018-10-07 20:04:08 -0400

    Spring Boot practical first commit

---
+++ spring-boot-practical/src/main/java/com/practical/springbootpractical/SpringBootPracticalApplication.java
@@ -0,0 +1,12 @@
+package com.practical.springbootpractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPracticalApplication.class, args);
+    }
+}
